6ba5d677c10e55639f41f426cc3a48fd165e619d
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-12-04 14:43:57 -0600

    AoC Day 4

---
+++ 04.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+
+def count_valid_passphrases(strings):
+    return len(list(filter(lambda s: is_valid_passphrase(s), strings)))
+
+def is_valid_passphrase(string):
+    words = string.split(" ")
+    return len(words) == len(set(words))
+
+if __name__ == '__main__':
+    print(count_valid_passphrases(["aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa"]))
+    print(count_valid_passphrases([s.strip() for s in open('04.input', 'r').read().splitlines()]))
