5be014e70486075fbf4e318e81af8534f8412ed3
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-12-05 10:12:35 -0600

    AoC Day 1.1

--- 01.py
+++ 01.py
@@ -1,25 +1,20 @@
 #!/usr/local/bin/python3
 
-def main(path):
-  f = open(path, 'r')
-  nums = [int(char) for char in list(f.read().strip())]
-  return inverse_captcha(nums)
-
 def inverse_captcha(nums):
   total = 0
   length = len(nums)
   for i, curr in enumerate(nums):
-    if i == length - 1:
-      nxt = nums[0]
-    else:
-      nxt = nums[i + 1]
-    if nxt == curr:
+    j = i + length // 2
+    if j >= length:
+      j = j - length
+    if nums[j] == curr:
       total = total + curr
   return total
 
 if __name__ == '__main__':
-  print(inverse_captcha([1, 1, 2, 2]))
-  print(inverse_captcha([1, 1, 1, 1]))
-  print(inverse_captcha([1, 2, 3, 4]))
-  print(inverse_captcha([9, 1, 2, 1, 2, 1, 2, 9]))
-  print(main('./01.input'))
+  print(inverse_captcha([1, 2, 1, 2]))
+  print(inverse_captcha([1, 2, 2, 1]))
+  print(inverse_captcha([1, 2, 3, 4, 2, 5]))
+  print(inverse_captcha([1, 2, 3, 1, 2, 3]))
+  print(inverse_captcha([1, 2, 1, 3, 1, 4, 1, 5]))
+  print(inverse_captcha([int(char) for char in list(open('01.input').read().strip())]))
