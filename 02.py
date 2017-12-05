d51dc89985441a3e5ed29897ab22a33cc71ed22b
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-12-05 13:38:59 -0600

    AoC Day 2.1

--- 02.py
+++ 02.py
@@ -7,19 +7,14 @@ def main(path):
     with open(path, 'r', newline='') as f:
         reader = csv.reader(f, delimiter="\t")
         for row in reader:
-            minimum, maximum = minmax([int(x) for x in row])
-            checksum = checksum + maximum - minimum
+            checksum = checksum + evenly_divisible_value_in([int(i) for i in row])
     return checksum
 
-def minmax(a):
-    maximum = a[0];
-    minimum = a[0];
-    for x in a:
-        if x > maximum:
-            maximum = x
-        elif x < minimum:
-            minimum = x
-    return (minimum, maximum)
+def evenly_divisible_value_in(a):
+    for i in a:
+        for j in a:
+            if i // j == i / j and j != i:
+                return (i // j)
 
 if __name__ == '__main__':
     print(main('./02.input.test'))
