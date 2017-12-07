7f62816df4fe30bc6ee232dc3645524ab58b95b4
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-12-07 08:27:56 -0600

    AoC Day 7.1

--- 07.rb
+++ 07.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 $programs = {}
 
 class Program
@@ -15,16 +13,36 @@ class Program
     @_aboves.map { |n| $programs[n] }
   end
 
-  def belows
-    $programs.values.select { |p| p.aboves.include?(self) }
+  def below
+    $programs.values.find { |p| p.aboves.include?(self) }
   end
 
   def to_s
-    name
+    "#{name}, #{depth}, #{weight}, #{total_weight}"
   end
 
   def inspect
-    name
+    to_s
+  end
+
+  def balanced?
+    siblings.all? { |s| s.total_weight == total_weight }
+  end
+
+  def siblings
+    if below
+      below.aboves - [self]
+    else
+      []
+    end
+  end
+
+  def total_weight
+    weight + aboves.sum { |a| a.total_weight }
+  end
+
+  def depth(i = 0)
+    below.nil? ? i : below.depth(i + 1)
   end
 end
 
@@ -38,4 +56,10 @@ STDIN.readlines.map { |line|
   )
 }
 
-puts $programs.values.find { |p| p.belows == [] }
+progs = $programs.values.select { |p| !p.balanced? }
+max_depth = progs.map(&:depth).max
+deepests = progs.select { |p| p.depth == max_depth }
+max_total_weight = deepests.map(&:total_weight).max
+unbalanced = deepests.find { |d| d.total_weight == max_total_weight }
+balanced = deepests.find { |d| d != unbalanced }
+puts unbalanced.weight - (unbalanced.total_weight - balanced.total_weight)
