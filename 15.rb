7e6e84ddcbfcfcb1870ca2b2d71923a7b6a4ac4e
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-12-14 23:36:00 -0600

    AoC Day 15.1

--- 15.rb
+++ 15.rb
@@ -1,7 +1,12 @@
+require 'pry'
+
 a_factor = 16807
 b_factor = 48271
 comb = 2147483647
 mask = 0xFFFF
+
+a_mod = 4
+b_mod = 8
 
 # a = 65
 a = 116
@@ -9,14 +14,26 @@ a = 116
 b = 299
 
 count = 0
-times = 40_000_000
+# times = 5
+times = 5_000_000
 
-times.times do
+as = []
+bs = []
+
+while as.length < times do
   a = (a * a_factor) % comb
+  as << a if a % a_mod == 0
+end
+
+while bs.length < times do
   b = (b * b_factor) % comb
-  count += 1 if a & mask == b & mask
+  bs << b if b % b_mod == 0
+end
+
+0.upto(times - 1) do |i|
+  count += 1 if (bs[i] & mask == as[i] & mask)
 end
 
 puts count
 
-# not 156044
+# not 85
