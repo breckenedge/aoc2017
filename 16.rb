2a3b417613f351ed6d3812db446a6b6ca231e1b6
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2017-12-16 01:40:44 -0600

    AoC Day 16.1

--- 16.rb
+++ 16.rb
@@ -1,52 +1,60 @@
 #!/usr/bin/ruby
-#
+
+require './circular_list'
 require 'pry'
 
 class Dance
   attr_reader :positions
 
-  def initialize(programs = 16)
-    @positions = programs.times.map { |i| (i + 97).chr }
+  def initialize(starting)
+    @positions = starting.split('')
   end
 
-  def spin(input)
-    times = input.sub('s', '').to_i
+  def spin(times)
     @positions.rotate!(-times)
   end
 
-  def exchange(input)
-    pos1, pos2 = input.sub('x', '').split('/').map(&:to_i)
+  def exchange(pos1, pos2)
     tmp = @positions[pos1]
     @positions[pos1] = @positions[pos2]
     @positions[pos2] = tmp
-  rescue
-    binding.pry
   end
 
-  def partner(input)
-    prog1, prog2 = input.sub('p', '').split('/')
+  def partner(prog1, prog2)
     pos1 = @positions.index(prog1)
     pos2 = @positions.index(prog2)
-    exchange("x#{pos1}/#{pos2}")
+    exchange(pos1, pos2)
   end
 end
 
 if __FILE__ == $0
   inputs = (ARGV.empty? ? DATA : ARGF).readlines.map(&:chomp).first.split(',')
-
   dance = Dance.new("giadhmkpcnbfjelo")
-  inputs.each do |input|
+  #dance = Dance.new("abcde")
+  moves = inputs.map { |input|
     case input[0]
     when 's'
-      dance.spin(input)
+      [:spin, input.sub('s', '').to_i]
     when 'x'
-      dance.exchange(input)
+      [:exchange, *input.sub('x', '').split('/').map(&:to_i)]
     when 'p'
-      dance.partner(input)
+      [:partner, *input.sub('p', '').split('/')]
     end
+  }
+
+  positions = []
+  repeat = 0
+
+  loop do
+    moves.each do |move|
+      dance.send(*move)
+    end
+    break if positions.index(dance.positions.join)
+    positions << dance.positions.join
+    repeat += 1
   end
 
-  puts dance.positions.join
+  puts CircularList.new(positions)[1_000_000_000 % repeat - 2] # -1 for initial and -1 for off-by-one :-(
 end
 
 
